f4e96337f0aa19651546f17860a44c8a6b6df731
Bidenfilang <1398026791@qq.com>
2018-11-02 09:49:16 +0800

    新增欢迎页面

---
+++ app/src/main/java/com/example/weibo/ui/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.weibo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.weibo.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
